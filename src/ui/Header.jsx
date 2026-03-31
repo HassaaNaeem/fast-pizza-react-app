72e61f911572457f351f4cfb6ecd7a9c7258b5ec
HassaaNaeem <hassaan282006@gmail.com>
2026-03-31 13:51:06 +0500

    Fetched Orders by id

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
 
 function Header() {
   return (
     <header>
       <Link to="/">Fast Pizza Co.</Link>
+      <SearchOrder></SearchOrder>
       <p>Name</p>
     </header>
   );
