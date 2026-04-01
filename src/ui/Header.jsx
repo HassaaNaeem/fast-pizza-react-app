358ffcba4fc67ffbe7ce30544b04477b6e2bc879
HassaaNaeem <hassaan282006@gmail.com>
2026-04-02 01:05:55 +0500

    Updated

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
 
 function Header() {
   return (
-    <header>
-      <Link to="/">Fast Pizza Co.</Link>
+    <header className="bg-yellow-500 uppercase">
+      <Link to="/" className="tracking-widest">
+        Fast Pizza Co.
+      </Link>
       <SearchOrder></SearchOrder>
-      <p>Name</p>
+      <Username />
     </header>
   );
 }
