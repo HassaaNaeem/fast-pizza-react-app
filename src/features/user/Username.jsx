b408b282f0d94405adf48c42fe86b5874381251f
HassaaNaeem <hassaan282006@gmail.com>
2026-04-02 19:37:10 +0500

    Added styles with Tailwind css

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Username() {
-  return <div className="text-sm font-semibold">Hassaan</div>;
+  return <div className="hidden text-sm font-semibold md:block">Hassaan</div>;
 }
 
 export default Username;
