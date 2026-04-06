e920383f761c8a4182bea34d93bbc95a2b28ec1a
HassaaNaeem <hassaan282006@gmail.com>
2026-04-06 22:04:26 +0500

    Fetched menu data using useFetcher

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -1,6 +1,6 @@
 // Test ID: IIDSAT
 
-import { useLoaderData } from "react-router-dom";
+import { useFetcher, useLoaderData } from "react-router-dom";
 import { getOrder } from "../../services/apiRestaurant.js";
 import {
   calcMinutesLeft,
@@ -8,9 +8,14 @@ import {
   formatDate,
 } from "../../utils/helper.js";
 import OrderItem from "./OrderItem.jsx";
+import { useEffect } from "react";
 
 function Order() {
   const order = useLoaderData();
+  const fetcher = useFetcher();
+  useEffect(() => {
+    if (!fetcher.data && fetcher.state == "idle") fetcher.load("/menu");
+  }, [fetcher]);
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
   const {
     id,
@@ -53,7 +58,15 @@ function Order() {
 
       <ul className="divide-y divide-stone-200 border-t border-b border-stone-200">
         {cart.map((item) => (
-          <OrderItem item={item} key={item.pizzaId} />
+          <OrderItem
+            item={item}
+            key={item.pizzaId}
+            isLoadingIngredients={fetcher.state == "loading"}
+            ingredients={
+              fetcher?.data?.find((el) => el.id == item.pizzaId).ingredients ??
+              []
+            }
+          />
         ))}
       </ul>
 
